520e121822541da3c0b07f5efc262b405cc124a3
stevelemor <35205326+stevelemor@users.noreply.github.com>
2018-03-22 01:24:16 +0800

    family_background
    family_background complete

---
+++ application/views/admin/employee_form/employee_family_background.php
@@ -0,0 +1,145 @@
+<div class="container-FLUID">
+		<div class="row">
+			<div class="col s12">
+				<h6>FAMILY BACKGROUND</h6>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="spouse_surname" name="spouse_surname" type="text" class="validate" >
+	              <label for="spouse_surname">SPOUSE'S SURNAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="spouses_first_name" name="spouses_first_name" type="text" class="validate" >
+	              <label for="spouses_first_name">SPOUSE'S FIRST NAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="spouses_middel_name" name="spouses_middel_name" type="text" class="validate" >
+	              <label for="spouses_middel_name">SPOUSE'S MIDDLE NAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="spouses_name_ext" name="spouses_name_ext" type="text" class="validate" >
+	              <label for="spouses_name_ext">SPOUSE'S NAME EXTENSION(JR., SR)</label>
+	            </div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s6">
+				<div class="input-field ">
+	              <input id="spouse_occupation" name="spouse_occupation" type="text" class="validate" >
+	              <label for="spouse_occupation">OCCUPATION</label>
+	            </div>
+			</div>
+			<div class="col s6">
+				<div class="input-field ">
+	              <input id="spouse_employer_name" name="spouse_employer_name" type="text" class="validate" >
+	              <label for="spouse_employer_name">EMPLOYEE/BUSINESS NAME</label>
+	            </div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s6">
+				<div class="input-field ">
+	              <input id="spouse_business_address" name="spouse_business_address" type="text" class="validate" >
+	              <label for="spouse_business_address">BUSINESS ADDRESS</label>
+	            </div>
+			</div>
+			<div class="col s6">
+				<div class="input-field ">
+	              <input id="spouse_telephone_no" name="spouse_telephone_no" type="number" class="validate" >
+	              <label for="spouse_telephone_no">TELEPHONE NO.</label>
+	            </div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="child_last_name" name="child_last_name" type="text" class="validate" >
+	              <label for="child_last_name">CHILD LAST NAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="child_first_name" name="child_first_name" type="text" class="validate" >
+	              <label for="child_first_name">CHILD FIRST NAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="child_mid_name" name="child_mid_name" type="text" class="validate" >
+	              <label for="child_mid_name">CHILD MIDDLE NAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="date_of_birth" name="date_of_birth" type="text" class="datepicker" >
+	              <label for="date_of_birth">DATE OF BIRTH<small>(mm/dd/yyyy)</small></label>
+	            </div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="fathers_surname" name="fathers_surname" type="text" class="validate" >
+	              <label for="fathers_surname">FATHERS SURNAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="fathers_first_name" name="fathers_first_name" type="text" class="validate" >
+	              <label for="fathers_first_name">FATHERS FIRST NAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="fathers_middle_name" name="fathers_middle_name" type="text" class="validate" >
+	              <label for="fathers_middle_name">FATHERS MIDDLE NAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="fathers_name_extension" name="fathers_name_extension" type="text" class="validate" >
+	              <label for="fathers_name_extension">NAME EXTENSION(JR., SR)</label>
+	            </div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="mothers_surname" name="mothers_surname" type="text" class="validate" >
+	              <label for="mothers_surname">MOTHERS SURNAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="mothers_first_name" name="mothers_first_name" type="text" class="validate" >
+	              <label for="mothers_first_name">MOTHERS FIRST NAME</label>
+	            </div>
+			</div>
+			<div class="col s3">
+				<div class="input-field ">
+	              <input id="mothers_middle_name" name="mothers_middle_name" type="text" class="validate" >
+	              <label for="mothers_middle_name">MOTHERS MIDDLE NAME</label>
+	            </div>
+			</div>
+		</div>
+	</div>
+
+
+<script type="text/javascript">
+	  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  });
+</script>
