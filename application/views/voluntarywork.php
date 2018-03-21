8d370d9897f506b8b9d079ed9d1a71b2e0d84a7e
chonching <marlontaguicana@gmail.com>
2018-03-22 01:09:05 +0800

    voluntarywork
    voluntarywork

---
+++ application/views/voluntarywork.php
@@ -0,0 +1,57 @@
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+
+  <div class="container-fluid">
+
+  	<div class="row">
+  		<div class="col s12">
+  			<p>Voluntary Work or Involvement in Civic/Non-Government/People/Voluntary Organizations</p>
+  		</div>
+
+     
+      <div class="col s12">
+        <center>
+        <p>INCLUSIVE DATES (mm/dd/yyyy)</p>
+        </center>
+      </div>
+
+  		<div class="col s5">
+  			<div class="input-field ">
+  				<input id="namenadd" name="namenadd" type="text" class="validate" >
+  				<label for="namenadd">NAME & ADDRESS OF ORGANIZATION (Write in full)</label>
+  			</div>
+  		</div>
+  		<div class="col s1">
+  			<div class="input-field ">
+  				<input id="from" name="from" type="text" class="validate" >
+  				<label for="from">FROM</label>
+  			</div>
+  		</div>
+      <div class="col s1">
+        <div class="input-field ">
+          <input id="to" name="to" type="text" class="validate" >
+          <label for="to">TO</label>
+        </div>
+      </div>
+  		<div class="col s2">
+  			<div class="input-field ">
+  				<input id="numofhours" name="numofhours" type="text" class="validate" >
+  				<label for="numofhours">NUMBER OF HOURS</label>
+  			</div>
+  		</div>
+  		<div class="col s3">
+  			<div class="input-field ">
+  				<input id="position" name="position" type="text" class="validate" >
+  				<label for="position">POSITION/NATURE OR WORK</label>
+  			</div>
+  		</div>
+
+  	</div>
+  </div>
