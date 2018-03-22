d6085094575358555f5da4a7adfb165a216df1fc
stevelemor <35205326+stevelemor@users.noreply.github.com>
2018-03-23 03:24:32 +0800

    a
    a

---
+++ application/views/employee/employee_document_requests.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="card row" style="margin-bottom: 0;">
+		<nav>
+			<div class="nav-wrapper blue">
+				<div class="col s11">
+					<a href="<?php echo base_url(); ?>Employee_document_requests" class="breadcrumb">Employee Document Requests</a>
+				</div>		
+			</div>
+		</nav>
+	</div>
+
+	<div class="col s12 card pad-lg">
+		 Employee Document Requests
+	</div>
+</div>
