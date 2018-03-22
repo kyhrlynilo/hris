91130ec36759fa4b504cf3897b372cb40f03ce80
stevelemor <35205326+stevelemor@users.noreply.github.com>
2018-03-22 21:58:34 +0800

    last na info na pinagawa mo

--- application/views/admin/employee_form/employee_other_info.php
+++ application/views/admin/employee_form/employee_other_info.php
@@ -30,6 +30,303 @@ crossorigin="anonymous"></script>
                 <input id="position_title" name="position_title" type="text" class="validate" placeholder="(Write in full / Do not abbreviate)" >
                 <label for="position_title">MEMBERSHIP IN ASSOCIATION / ORGANIZATION </label>
             </div>
-        </div>  
+        </div> 
     </div>
+    <div class="row">
+            <div class="col s7">
+                    <p>
+                        Are you related by consanguinity or affinity to the appointing or recommending authority, or to the chief of bureau of office or to the person who has immediate supervision over you in the Office,<br>
+                        Bureau or Department where you will be appointed.<br>
+                    </p>
+                    <p>a. within the third degree?</p>
+                    <p>b. within the fourth degree (for local Goverment unit - Career Employees)?</p>
+            </div>
+            <div class="col s4">
+                <p></p><br><br><br>
+                <p>
+                  <input name="group1" type="radio" id="test1" />
+                  <label for="test1">YES</label>
+               
+                  <input name="group1" type="radio" id="test2" />
+                  <label for="test2">NO</label>
+                </p>
+                <p>
+                  <input name="group2" type="radio" id="test3" />
+                  <label for="test3">YES</label>
+               
+                  <input name="group2" type="radio" id="test4" />
+                  <label for="test4">NO</label>
+                </p>
+                <div class="input-field ">
+                  <input id="" name="" type="text" class="validate" >
+                  <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                    <p>a. Have you ever been found guilty of any administrative offense?</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group3" type="radio" id="test5" />
+                  <label for="test5">YES</label>
+               
+                  <input name="group3" type="radio" id="test8" />
+                  <label for="test6">NO</label>
+                </p>
+                <div class="input-field ">
+                  <input id="" name="" type="text" class="validate" >
+                  <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                    <p>b. Have you been criminally charged before any court?</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group4" type="radio" id="test7" />
+                  <label for="test7">YES</label>
+               
+                  <input name="group3" type="radio" id="test8" />
+                  <label for="test8">NO</label>
+                </p>
+                <label for="">IF YES, GIVE DETAILS</label>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="datepicker" >
+                  <label for="">DATE FIELD</label>
+                </div>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                  <label for="">STATUS CASE/S</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Have you ever been convicted of any crime or violation of any law, decree, ordinance or regulation by any court or tribunal?</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group5" type="radio" id="test9" />
+                  <label for="test9">YES</label>
+               
+                  <input name="group3" type="radio" id="test10" />
+                  <label for="test10">NO</label>
+                </p>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                   <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Have you ever been separated from the service in any of the following modes: resignation, retirement, dropped from the rolls, dismissal, termination, end of term, finished contract or phased out (abolition) in the public or private sector?</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group6" type="radio" id="test11" />
+                  <label for="test11">YES</label>
+               
+                  <input name="group6" type="radio" id="test12" />
+                  <label for="test12">NO</label>
+                </p>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                   <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Have you ever been a candidate in a national or local held within the last year (except Barangap election)?</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group7" type="radio" id="test13" />
+                  <label for="test13">YES</label>
+               
+                  <input name="group7" type="radio" id="test14" />
+                  <label for="test14">NO</label>
+                </p>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                   <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Have you resigned from the government service during the three (3)-month period before the last election to promote/actively campaign for national or local candidate</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group8" type="radio" id="test15" />
+                  <label for="test15">YES</label>
+               
+                  <input name="group8" type="radio" id="test16" />
+                  <label for="test16">NO</label>
+                </p>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                   <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Have you acquired the status of an immigrant or permanent resident of another country</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group9" type="radio" id="test17" />
+                  <label for="test17">YES</label>
+               
+                  <input name="group9" type="radio" id="test18" />
+                  <label for="test18">NO</label>
+                </p>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                   <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Pursuant to:(a) Indigenous People's Act(RA 8371); (b) Magna Carta for Disable Persons(RA 7277); (c) Solo Parents Welfare Act of 2000 (RA 8972), please answer the following items</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Are you a member of any indigenous group?</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group10" type="radio" id="test19" />
+                  <label for="test19">YES</label>
+               
+                  <input name="group10" type="radio" id="test20" />
+                  <label for="test20">NO</label>
+                </p>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                   <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Are you a person with disability?</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group11" type="radio" id="test21" />
+                  <label for="test21">YES</label>
+               
+                  <input name="group11" type="radio" id="test22" />
+                  <label for="test22">NO</label>
+                </p>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                   <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s7">
+                <p>Are you a solo parent?</p>
+            </div>
+            <div class="col s4">
+                <p>
+                  <input name="group12" type="radio" id="test23" />
+                  <label for="test23">YES</label>
+               
+                  <input name="group12" type="radio" id="test24" />
+                  <label for="test24">NO</label>
+                </p>
+                <div class="input-field">
+                  <input id="" name="" type="text" class="validate" >
+                   <label for="">IF YES, GIVE DETAILS</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12">
+                <h6>
+                    REFERENCES <small>Person not related by consanquinity or affinity to applicant/appointee</small> 
+                </h6>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s2">
+                <div class="input-field">
+                   <input id="reference_surname" name="reference_surname" type="text" class="validate" >
+                   <label for="reference_surname">REFERENCE SURNAME</label>
+                </div>
+            </div>
+            <div class="col s2">
+                <div class="input-field">
+                   <input id="reference_first_name" name="reference_first_name" type="text" class="validate" >
+                   <label for="reference_first_name">REFERENCE FIRST NAME</label>
+                </div>
+            </div>
+            <div class="col s2">
+                <div class="input-field">
+                   <input id="reference_middle_name" name="reference_middle_name" type="text" class="validate" >
+                   <label for="reference_middle_name">REFERENCE MIDDLE NAME</label>
+                </div>
+            </div>
+            <div class="col s3">
+                <div class="input-field">
+                   <input id="reference_address" name="reference_address" type="text" class="validate" >
+                   <label for="reference_address">ADDRESS</label>
+                </div>
+            </div>
+            <div class="col s2">
+                <div class="input-field">
+                   <input id="references_tel_no" name="references_tel_no" type="text" class="validate" >
+                   <label for="references_tel_no">TELEPHONE NO.</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12">
+                <p>I declare under oath that I have personally accomplished this Personal Data Sheet which is a true, correct and complete statement pursuant to the provisions of pertinent laws, rules and regulations of the Republic of the Philippines. I authorize the agency head/authorized representative to verify/validate the contents stated herein.
+                I agree that any misrepresentation made in this document and its attachments shall cause the filling of administrative/criminal case/s againts me.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s6">
+                <p>Government Issued ID<small>(i.e Passport, GSIS, SSS, PRC, Driver's License, Etc)</small></p>
+                <p>PLEASE INDICATE ID Number and Date of Issuance</p>
+                <div class="row">
+                    <div class="col s12">
+                        <div class="input-field">
+                           <input id="government_issued_id" name="government_issued_id" type="text" class="validate" >
+                           <label for="government_issued_id">GOVERNMENT ISSUED ID</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <div class="input-field">
+                           <input id="id_license_passport_no" name="id_license_passport_no" type="text" class="validate" >
+                           <label for="id_license_passport_no">ID/LICENSE/PASSPORT NO</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <div class="input-field">
+                           <input id="date_place_of_issuance" name="date_place_of_issuance" type="text" class="validate" >
+                           <label for="date_place_of_issuance">DATE/PLACE OF ISSUANCE</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </div>
