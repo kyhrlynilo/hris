a0e4aeb270480dda5483e6af105c09cd42a4e56a
chonching <marlontaguicana@gmail.com>
2018-03-22 23:33:16 +0800

    emp payslip3
    emp payslip3

---
+++ application/views/admin/employee_form/employee_payslip3.php
@@ -0,0 +1,88 @@
+<!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+<script
+src="https://code.jquery.com/jquery-3.3.1.js"
+integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+crossorigin="anonymous"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+
+<div class="container-fluid">
+
+    <div class="row">
+<table border="1" cellspacing="0" cellpadding="0" > 
+<tbody>
+<tr>
+<td colspan="4" valign="top" style="text-align:center"><p><b>EMPLOYEE PAY SLIP</b></p>
+
+<p><b>UNIVERSITY OF MAKATI</b></p></td>
+</tr>
+
+<tr>
+<td valign="top" ><p>NAME OF EMPLOYEE</p></td>
+<td valign="top" ><p>TEVES, ROMEL M.</p></td>
+<td colspan="2" valign="top" ><p>MARCH 16-30, 2018</p></td>
+</tr>
+
+<tr>
+<td colspan="2" valign="top" ><p><b>EARNINGS</b></p></td>
+<td colspan="2" valign="top" ><p><b>DEDUCTIONS</b></p></td>
+</tr>
+
+<tr>
+<td valign="top" ><p><b>BASIC</b></p>
+
+<p><b>PERA</b></p>
+
+<p><b>ACA</b></p>
+
+<p><b>OTHER EARNINGS</b></p></td>
+<td valign="top" ><p>14,885.00</p>
+
+<p>909.10</p></td>
+<td valign="top" ><p><b>W/ Tax</b></p>
+
+<p><b>PAG-IBIG</b></p>
+
+<p><b>PHIL. HEALTH</b></p>
+
+<p><b>GSIS Personal Share</b></p>
+
+<p><b>E-Cash</b></p>
+
+<p><b>MACEMCO</b></p>
+
+<p><b>UMEMPC</b></p>
+
+<p><b>Policy</b></p>
+
+<p><b>PVP</b></p>
+
+<p><b>ABSENCES W/O PAY</b></p></td>
+<td valign="top" ><p>100.00</p>
+
+<p>400.00</p>
+
+<p>2,947.23</p></td>
+</tr>
+
+<tr>
+<td valign="top" ><p><b>TOTAL EARNINGS</b></p></td>
+<td valign="top" ><p><b>15,794.1</b></p></td>
+<td valign="top" ><p><b>TOTAL DEDUCTIONS</b></p></td>
+<td valign="top" ><p><b>3,447.23</b></p></td>
+</tr>
+
+<tr>
+<td colspan="2" valign="top" ><p>PAYMENT SCHEME AUTHENTICATED BY</p></td>
+<td valign="top" ><p><b>NET EARNINGS</b></p></td>
+<td valign="top" ><p><b>12,346.87</b></p></td>
+</tr>
+
+</tbody>
+</table>
+
+
+    </div>
+</div>
