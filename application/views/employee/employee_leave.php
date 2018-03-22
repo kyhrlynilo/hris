1bc043d723e40febce57515345d4fb6454bc4821
kyhrlynilo <kyleharleylumbangnilo@gmail.com>
2018-03-23 03:24:35 +0800

    breadcrumbs

---
+++ application/views/employee/employee_leave.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="card row" style="margin-bottom: 0;">
+		<nav>
+			<div class="nav-wrapper blue">
+				<div class="col s11">
+					<a href="<?php echo base_url(); ?>Employee_leave" class="breadcrumb">Leave</a>
+				</div>		
+			</div>
+		</nav>
+	</div>
+
+	<div class="col s12 card pad-lg">
+		
+	</div>
+</div>
