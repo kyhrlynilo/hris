cf6a78cf4c7d7161a65bd6c42016ed7760858d07
stevelemor <35205326+stevelemor@users.noreply.github.com>
2018-03-23 00:00:33 +0800

    ALL REPORT
    reports

---
+++ application/views/reports/reports_hrmo_certificate_employement_compensation.php
@@ -0,0 +1,68 @@
+<div class="row">
+			<div class="col s12">
+				<div align="center">
+					<h4><b>HUMAN RESOURCES MANAGEMENT OFFICE</b></h4>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<div align="right">
+					February 13,2018
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<div align="center">
+					<h5><u><b>CERTIFICATE OF EMPLOYMENT AND COMPENSATION</b></u></h5>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This is to certify that <b>DR. DANILO C SABADO</b> is a permanent employee of this University from November 12, 2003 to present. He is holding the position of Associate Professor V with a annual income broken down as follows:</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s1"></div>
+			<div class="col s5">
+				<p>Basic Salary</p>
+				<p>Personnel Economic Relief Allowance</p>
+				<p>Mid-Year Bonus</p>
+				<p>13th Month Pay</p>
+				<p>Fringe Benefit</p>
+				<p>Cash Gift</p>
+				<p>Clothing Allowance</p>
+				<p>Incentives</p>
+
+			</div>
+			<div class="col s4">
+				<div align="right">
+					<p>P 787,248.00</p>
+					<p>24,000.00</p>
+					<p>65,604.00</p>
+					<p>65,604.00</p>
+					<p>65,604.00</p>
+					<p>5,000.00</p>
+					<p>5,000.00</p>
+					<p>10,000.00</p>
+					<p>_____________</p>
+					<p>P 1,027,860.00</p>
+				</div>
+			</div>
+			<div class="col s2"></div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This certification is issued upon his request for whatever legal purpose it may serve.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s7"></div>
+			<div class="col s5">
+				<b>ENGR. SHIELA I. VILLANUEVA</b>
+				<br>Head
+			</div>
+		</div>
+		<br><br><br><br>	
