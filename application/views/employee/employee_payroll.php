e6cd80f08c909515cfcdefaa0a18ef6b63eb8b92
kyhrlynilo <kyleharleylumbangnilo@gmail.com>
2018-03-23 04:27:52 +0800

    latest
    latest

--- application/views/employee/employee_payroll.php
+++ application/views/employee/employee_payroll.php
@@ -10,7 +10,82 @@
 	</div>
 
 	<div class="col s12 card pad-lg">
-	employee payroll
+		<div class="container-fluid">
+
+			<div class="row">
+				<div class="col s12" style="text-align: center;">
+					<p>UNIVERSITY OF MAKATI</p>
+				</div>
+
+				<table class="table-bordered striped"  > 
+					<tbody>
+						<tr>
+							<td valign="top" ><p>NAME OF EMPLOYEE</p></td>
+							<td valign="top" ><p>TEVES, ROMEL M.</p></td>
+							<td colspan="2" valign="top" ><p>MARCH 16-31, 2018</p></td>
+						</tr>
+
+						<tr>
+							<td colspan="2" valign="top" ><p><b>EARNINGS</b></p></td>
+							<td colspan="2" valign="top" ><p><b>DEDUCTIONS</b></p></td>
+						</tr>
+
+						<tr>
+							<td valign="top" ><p><b>Basic</b></p>
+
+								<p><b>PERA</b></p>
+
+								<p><b>ACA</b></p></td>
+								<td valign="top" ><p><b>5,957.00</b></p>
+
+									<p><b>1,000.00</b></p></td>
+									<td valign="top" ><p><b>PAG-IBIG</b></p>
+
+										<p><b>W/ Tax 2017</b></p>
+
+										<p><b>PHIL. HEALTH</b></p>
+
+										<p><b>GSIS Personal Share</b></p>
+
+										<p><b>EDUCATIONAL LOAN</b></p>
+
+										<p><b>UMEMPC</b></p>
+
+										<p><b>MACEMCO</b></p>
+
+										<p><b>Gas</b></p>
+
+										<p><b>ABSENCES W/O PAY</b></p></td>
+										<td valign="top" ><p><b>100.00</b></p>
+
+											<p><b>168.82</b></p>
+
+											<p><b>1,072.26</b></p>
+
+											<p><b>2,256.48</b></p>
+
+											<p><b>2,863.44</b></p></td>
+										</tr>
+
+										<tr>
+											<td valign="top" ><p><b>TOTAL EARNINGS</b></p></td>
+											<td valign="top" ><p><b>6,957.00</b></p></td>
+											<td valign="top" ><p><b>TOTAL DEDUCTIONS</b></p></td>
+											<td valign="top" ><p><b>6,457.00</b></p></td>
+										</tr>
+
+										<tr>
+											<td colspan="2" valign="top" ><p><b>PAYMENT SCHEME AUTHENTICATED BY</b></p></td>
+											<td valign="top" ><p><b>NET EARNINGS:</b></p></td>
+											<td valign="top" ><p><b>500.00</b></p></td>
+										</tr>
+
+									</tbody>
+								</table>
 
 
-	</div>
+
+							</div>
+						</div>
+
+					</div>
