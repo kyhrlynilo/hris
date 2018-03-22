c877e86fe85b952e56a826bb23341d5c6f910608
chonching <marlontaguicana@gmail.com>
2018-03-23 03:46:51 +0800

    s
    s

--- application/views/employee/employee_time_keeping.php
+++ application/views/employee/employee_time_keeping.php
@@ -10,6 +10,109 @@
 	</div>
 
 	<div class="col s12 card pad-lg">
-			Employee Time Keeping
+		<div class="col s12 card pad-lg">
+		<div class="row">
+			<div class="col s12">
+				<div align="center">
+					<h4><b>DAILY TIME RECORD</b></h4>	
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<div align="center">
+					NAIM, ALI<br>
+					REGULAR - CCS
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<hr>
+				<div align="center">
+					(Name)
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<div align="center">
+					<h5><b>For the month of February 01-15, 2018</b></h5>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<table class="bordered striped">
+					<thead>
+						<tr>
+							<th rowspan="2">DAY</th>
+							<th colspan="2"><div align="center">A.M</div></th>
+							<th colspan="2"><div align="center">P.M</div></th>
+							<th rowspan="2">HG</th>
+							<th rowspan="2">HU</th>
+							<th rowspan="2">Remarks</th>
+						</tr>
+						<tr>
+							<th><div align="center">IN</div></th>
+							<th><div align="center">OUT</div></th>
+							<th><div align="center">IN</div></th>
+							<th><div align="center">OUT</div></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1 - Thu</td>
+							<td>11:49:00</td>
+							<td>13:00:00</td>
+							<td>14:00:00</td>
+							<td>18:00:00</td>
+							<td>5.0</td>
+							<td>6.0</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>2 - Fri</td>
+							<td>11:49:00</td>
+							<td>13:00:00</td>
+							<td>14:00:00</td>
+							<td>18:00:00</td>
+							<td>5.0</td>
+							<td>6.0</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>3 - Sat</td>
+							<td>11:49:00</td>
+							<td>13:00:00</td>
+							<td>14:00:00</td>
+							<td>18:00:00</td>
+							<td>5.0</td>
+							<td>6.0</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td colspan="5"><div align="right">TOTAL:</div></td>
+							<td>0.00</td>
+							<td>5.93</td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<p>I certify on my honor that the above is a true and correct report of the hours of work performed, record of whic was made daily at the time of arrival and departure from office</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col s12">
+				<div align="center">
+					<h5>NAIM ALI</h5>	
+				</div>	
+			</div>
+		</div>
+	</div>
 	</div>
 </div>
