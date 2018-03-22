72d2eb26673ca501568c6eb1c1e4f4aedf293ddc
chonching <marlontaguicana@gmail.com>
2018-03-23 02:40:13 +0800

    asd
    asd

--- application/controllers/Admin_leave.php
+++ application/controllers/Admin_leave.php
@@ -7,9 +7,9 @@ class Admin_leave extends CI_Controller {
 	public function index()
 	{
 		$data['title'] = "Login";
-		$this->load->view('template/header',$data);
+		$this->load->view('admin/template/header',$data);
 		$this->load->view('admin/admin_leave',$data);
-		$this->load->view('template/footer',$data);
+		$this->load->view('admin/template/footer',$data);
 	}
 /*scds*/
 
