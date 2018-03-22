dbe2a2fe3bc13f3eda2db4f4bdadb5c6697eb00f
chonching <marlontaguicana@gmail.com>
2018-03-23 02:31:03 +0800

    admin leave
    admin leave

--- application/controllers/Admin_leave.php
+++ application/controllers/Admin_leave.php
@@ -1,69 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin extends CI_Controller {
+class Admin_leave extends CI_Controller {
 
 
 	public function index()
 	{
-		if( !$this->ion_auth->logged_in() ) 
-		{
-			redirect('Admin/login', 'refresh');
-		}
-	}
-
-	public function login()
-	{
 		$data['title'] = "Login";
-		$this->load->view('admin/login',$data);
-	}
-
-	public function authenticate()
-	{
-		$title = "";
-		$text = "";
-		$icon = "";
-		$button = "Okay";
-		try
-		{
-			$params = $this->input->post("data",true);
-			$data 	= $this->get_params($params);
-			
-			$required_fields = array('email','password');		
-			$this->validate_data($required_fields,$data);	
-
-			if ($this->ion_auth->login($data['email'], $data['password'], FALSE /*$remember*/))
-			{
-				$this->session->set_flashdata('message', $this->ion_auth->messages());
-				$title = "Authentication successful!";
-				$text = "Hello! Admin";
-				$icon = "success";
-				$button = FALSE;
-			}
-			else
-			{
-				$this->session->set_flashdata('message', $this->ion_auth->errors());
-				$title = "Authentication failed!";
-				$icon = "error";
-				$text = "Email and password do not match!";
-			}
-
-		}
-		catch(Exception $e)
-		{
-			$title = "Error";
-			$text = $e->getMessage();
-			$icon = "error";
-		}
-
-
-		$result = array(
-			"title" => $title , 
-			"text" => $text ,
-			"icon" => $icon ,
-			"button" => $button
-		);	
-		echo json_encode($result);		
+		$this->load->view('template/header',$data);
+		$this->load->view('admin/admin_leave',$data);
+		$this->load->view('template/footer',$data);
 	}
 
 
