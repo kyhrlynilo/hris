d5819a5e4cad27a27f561c45d5a877903b13ecd5
kyhrlynilo <kyleharleylumbangnilo@gmail.com>
2018-03-23 03:44:43 +0800

    sample views
    sample views

--- application/views/employee/form_leave.php
+++ application/views/employee/form_leave.php
@@ -1,13 +1,8 @@
-  <!-- Compiled and minified CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
-  <!-- Compiled and minified JavaScript -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-  
-  <style type="text/css">
-  td{
-  	border: 1px solid black;
-  }
+<style type="text/css">
+td{
+	border: 1px solid black;
+}
 </style>
 
 <div class="container">
