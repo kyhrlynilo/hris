f0ba9a106a5cef88dad3c0fa1c8eebf7ba787187
chonching <marlontaguicana@gmail.com>
2018-04-04 21:12:05 +0800

    leave request
    fix pa yung email address

--- application/views/admin/admin_leave.php
+++ application/views/admin/admin_leave.php
@@ -1,15 +1,86 @@
+
+<script type="text/javascript">
+  $('#admin_leave').addClass('active');
+</script>
+
+
 <div class="row">
 	<div class="card row" style="margin-bottom: 0;">
 		<nav>
 			<div class="nav-wrapper blue">
 				<div class="col s11">
-					<a href="<?php echo base_url(); ?>Admin_leave" class="breadcrumb">Leave</a>
+					<a href="<?php echo base_url(); ?>Admin_leave" class="breadcrumb">Leave Request</a>
 				</div>		
 			</div>
 		</nav>
 	</div>
 
 	<div class="col s12 card pad-lg">
-		Sample Data
+		
+		<table id="table_leave" class="table striped highlight">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Type</th>
+					<!-- <th>Reason</th>
+					<th>Start Date</th>
+					<th>End Date</th>
+					<th>Leave Hours</th>
+					<th>Total</th> -->
+					<th>Action</th>
+				</tr>
+			</thead>
+			<tfoot>
+				<tr>
+					<th>Name</th>
+					<th>Type</th>
+					<!-- <th>Reason</th>
+					<th>Start Date</th>
+					<th>End Date</th>
+					<th>Leave Hours</th>
+					<th>Total</th> -->
+					<th >Action</th>
+				</tr>
+			</tfoot>
+			<tbody>
+				<?php 
+				if($fetch_data->num_rows()>0){
+					foreach ($fetch_data->result() as $row) {
+						?>
+						<tr>
+							<td><?php echo $row->last_name ." ". $row->first_name .", ". $row->mid_name ?></td>
+							<td><?php echo $row->leave_type ?></td>
+							<!-- <td><?php echo $row->reason ?></td>
+							<td><?php echo $row->date_from ?></td>
+							<td><?php echo $row->date_to ?></td>
+							<td><?php echo $row->leave_hr_day ?></td>
+							<td><?php echo $row->total_hrs ?></td> -->
+							<td> 
+								<a href="<?php echo base_url(); ?>Admin_leave/employee_leave_request/<?php echo $row->id; ?>" class="btn green darken-2">View</a>
+							</td>
+						</tr>
+						<?php
+					}
+
+				}else{
+					?>
+					<tr>
+						<td colspan="3">No data found</td>
+					</tr>
+					<?php
+				}
+				?>
+			</tbody>
+		</table>
+	
 	</div>
 </div>
+
+<script type="text/javascript">
+	$(document).ready( function (){
+		$('#table_leave').dataTable();
+		$('select').material_select();
+
+	});
+
+</script>
--- application/views/employee/employee_leave_request.php
+++ application/views/employee/employee_leave_request.php
@@ -10,13 +10,10 @@
 		</nav>
 	</div>
 
-	<div class="col s12 card pad-lg">
-		<div class="container">
-			<div class="row" style="text-align: center;">
-				<h4>REQUEST FOR COMPENSATORY TIME-OFF</h4>
-			</div>
-<form method="post" id="admin_time_keeping_form" action="<?php echo base_url()?>Admin_time_keeping/form_validation">
+	<form method="post" id="employee_leave_request_form">
 
+		<input type="hidden" name="hdn_email_address" id="hdn_email_address" value="marlontaguicana@gmail.com">
+		<div class="col s12 card pad-lg">
 			<div class="row">
 				<div class="col s4" >
 					<label for="leave_type">Type</label>
@@ -28,78 +25,69 @@
 			</div>
 
 			<div class="row">
-				<div class="col s3">
-					<label for="emp_status">Employment Status</label>
-					<select id="emp_status" name="emp_status">
-						<option value="<?php echo ES_R; ?>"><?php echo ES_R; ?></option>
-						<option value="<?php echo ES_C; ?>"><?php echo ES_C; ?></option>
-						<option value="<?php echo ES_PT; ?>"><?php echo ES_PT; ?></option>
-						<option value="<?php echo ES_JO; ?>"><?php echo ES_JO; ?></option>
-					</select>
+				<div class="input-field col s12">
+					<textarea id="reason" name="reason" class="materialize-textarea"></textarea>
+					<label for="reason">Reason</label>
 				</div>
 			</div>
-			<div class="row">
-				<div class="col s12"  >
-					<div class="input-field ">
-						<input id="cs_id_no" name="cs_id_no" type="text" class="validate" >
-						<label for="cs_id_no">Reason</label>
-					</div>
-				</div>
-			</div>
+
+
 			<div class="row">
 				<div class="col s4" >
 					<div class="input-field ">
-						<input id="birth_date" name="birth_date" type="text" class="datepicker" >
-						<label for="birth_date">Start Date: <small>(mm/dd/yyyy)</small></label>
+						<input id="date_from" name="date_from" type="text" class="datepicker" >
+						<label for="date_from">Start Date: <small>(mm/dd/yyyy)</small></label>
 						<span class="text-danger"><?php echo form_error("date_from");?></span>
 					</div>
 				</div>
 				<div class="col s4" >
 					<div class="input-field ">
-						<input id="birth_date" name="birth_date" type="text" class="datepicker" >
-						<label for="birth_date">End Date: <small>(mm/dd/yyyy)</small></label>
+						<input id="date_to" name="date_to" type="text" class="datepicker" >
+						<label for="date_to">End Date: <small>(mm/dd/yyyy)</small></label>
 						<span class="text-danger"><?php echo form_error("date_to");?></span>
 					</div>
 				</div>
 				<div class="col s4" >
 					<div class="input-field ">
-						<input id="cs_id_no" name="cs_id_no" type="text" class="validate" >
-						<label for="cs_id_no">Leave Hours (per Day)</label>
+						<input id="leave_hr_day" name="leave_hr_day" type="text" class="validate" >
+						<label for="leave_hr_day">Leave Hours (per Day)</label>
 					</div>
 				</div>
 			</div>
-			
 
+			<div class="row">
+				<div class="col s12">
+					<center>
+						<button type="submit" id="id_send" name="id_send" class="btn waves-effect waves-light green"> 
+							Send Request
+						</button>
+						<a href="<?php echo base_url(); ?>Employee_leave" type="button" class="btn waves-effect waves-light orange">    
+							Cancel
+						</a>
+					</center>
+				</div>
+			</div>
 
-<div class="row" style="margin-top: 50px;border: 1px solid black; padding: 10px;">
-		<div class="col s12 center">
-			<button class="btn blue">SUBMIT</button>
-			<button type="clear" class="btn orange">CLEAR</button>
 		</div>
-	</div>
-
-</form>			
-		</div>
-	</div>
-
+	</div> 
+	
+</form>
 </div>
+
+
 <script type="text/javascript">
-
-  $(document).ready(function(){
-    populateCountries("country");
-    $('select').material_select();
-
-
-    $('.datepicker').pickadate({
+	$(document).ready(function(){
+		$('select').material_select();
+		$('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15, // Creates a dropdown of 15 years to control year,
         today: 'Today',
         clear: 'Clear',
         close: 'Ok',
         closeOnSelect: false // Close upon selecting a date,
-      });
+    });
 
-    $('.timepicker').pickatime({
+		$('.timepicker').pickatime({
         default: 'now', // Set default time: 'now', '1:30AM', '16:30'
         fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
         twelvehour: false, // Use AM/PM or 24-hour format
@@ -109,56 +97,44 @@
         autoclose: false, // automatic close timepicker
         ampmclickable: true, // make AM PM clickable
         aftershow: function(){} //Function for after opening timepicker
-      });
+    });
+	});
 
-  });
+/*	IO.setSubmitScript({
+		form_id : "employee_leave_request_form",
+		button_id : "id_send",
+		data_receiver_url : base_url + "Employee_leave/process_leave" ,
+		redirect_url : base_url + "employee_leave"
+    });	*/
 
-  function citizenship_checker()
-  {
-    if($('#cb_fil').is(':checked'))
-    { 
-      $('.typetoggle').hide();
-      $("#country").val("");
-      $('#cb_birth').removeAttr("disabled");
-      $('#cb_natural').removeAttr("disabled");
-      $('.select-dropdown').attr("disabled",true);  
-      $('#cb_birth').prop("checked",true);
-    }
 
-    if(!$('#cb_fil').is(':checked'))
-    {  
-      $('.typetoggle').show();
-      $('#cb_birth').attr("disabled",true);
-      $('#cb_natural').attr("disabled",true);
-      $('.select-dropdown').removeAttr("disabled",true);
-      $('#cb_birth').prop("checked",false);
-      $('#cb_natural').prop("checked",false);
+	$('#employee_leave_request_form').on('submit', function(e){
+		e.preventDefault();
+		var data = $(this).serialize();
+		var baseurl = "Employee_leave/process_leave"
+		$.ajax({
+			url: "<?php echo base_url() ?>" +baseurl,
+			type: 'POST',
+			data: data,
+			success: function(data){
+				if($.isEmptyObject(data)){
+					swal("Success!", "Employee has been added!", "success").then(
+						function(){
+							$('#employee_leave_request_form')[0].reset();
+							window.location ="<?php echo base_url() ?>/Employee_leave";
+						})
+					
+				}else{
+					var text = $(data).text();
+					swal("Error!",text, "error")
+				}  
+			},
+			error: function(){
+				alert('error');
+			}
+		});
+	});
 
-    }
 
-    $('select').material_select();
-  }
-
-  $(document).ready(function(){  
-    $('#email_address').change(function(){  
-     var email_address = $('#email_address').val();  
-
-     if(email_address != '')  
-     {
-       $('#email_result').show();
-       $.ajax({  
-         url:"<?php echo base_url(); ?>Admin_time_keeping/check_email_avalibility",  
-         method:"POST",  
-         data:{email_address:email_address},  
-         success:function(data){  
-          $('#email_result').html(data);  
-        }  
-      });  
-     } 
-     else{
-      $('#email_result').hide();
-    } 
-  });  
-  });  
 
 </script>
