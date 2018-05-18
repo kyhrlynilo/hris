81034b255032cb0a289e772e72ec63500ffa81c2
chonching <marlontaguicana@gmail.com>
2018-05-18 17:37:10 +0800

    Fix admin leave request view
    Fix admin leave request view

--- application/views/admin/admin_leave.php
+++ application/views/admin/admin_leave.php
@@ -10,8 +10,6 @@
 			<div class="nav-wrapper blue">
 				<div class="col s11">
 					<a href="<?php echo base_url(); ?>Admin_leave" class="breadcrumb">Leave Request</a>
-
-					<!-- <a href="<?php echo base_url(); ?>Admin_employee_leave_credit_points" class="breadcrumb">Employee Leave Credit Points</a> -->
 				</div>		
 			</div>
 		</nav>
--- application/views/admin/admin_leave_request_view.php
+++ application/views/admin/admin_leave_request_view.php
@@ -17,7 +17,8 @@ p{
 			</div>
 		</nav>
 	</div>
-	
+
+
 	<div class="col s12 card pad-lg" style="margin-bottom: 0;">
 		<form method="post"  id="admin_leave_request_form">
 			<?php 
@@ -30,8 +31,8 @@ p{
 			<br/>
 
 			<div class="row">
-					<div class="col s3">
-						<label><p>1. AGENCY<p></label>
+				<div class="col s3">
+					<label><p>1. AGENCY<p></label>
 						<label><p><b><?php echo $details->agency ;?></b></p></label>
 					</div>
 
@@ -43,9 +44,9 @@ p{
 					<div class="col s6">
 						<label for="full_name"><p>2. NAME (Surname, Given Name, M.I.)<p></label>
 							<label><p><b><?php echo $details->full_name ;?></b></p></label>
-					</div>
+						</div>
 
-			</div>
+					</div>
 
 					<div class="row">
 
@@ -72,52 +73,52 @@ p{
 					</div>
 
 					<div class="row">
-
 						<div class="col s6">
 							<p>6. DETAILS OF APPLICATION <br/>
 								6.a). TYPE OF LEAVE
 							</p>
 							<div class="col s12">
 								<p>
-									<input name="leave_type" type="checkbox" <?php echo $details->leave_type ==  VL ?  "checked" : ""; ?> />
+									<input type="checkbox" <?php echo $details->leave_type ==  VL ?  "checked" : ""; ?> />
 									<label><?php echo VL;?></label>
 								</p>
 							</div>
 							<div class="col s12">
 								<p>
-									<input name="leave_type" type="checkbox"  <?php echo $details->leave_type == TSE ?  "checked" : ""; ?>  />
+									<input type="checkbox"  <?php echo $details->leave_type == TSE ?  "checked" : ""; ?>  />
 									<label><?php echo TSE;?></label>
 								</p>
 							</div>
 
 							<div class="col s5">
 								<p>
-									<input name="leave_type" type="checkbox" <?php echo $details->leave_type == OTH ?  "checked" : ""; ?> />
-									<label><?php echo OTH;?>&nbsp;(Specify)</label>
+									<input type="checkbox" <?php echo $details->leave_type == OTH ?  "checked" : ""; ?> />
+									<label><?php echo OTH;?>&nbsp(Specify)</label>
 								</p>
 							</div>
+
 							<div class="col s7">
 								<div style="margin-top:-18px;">
-									<label for="reason">&nbsp;</label>
-									<label id="reason" name="reason"><p><b>&nbsp;
+									<label for="reason">&nbsp</label>
+									<label id="reason" name="reason"><p><b>&nbsp
 										<?php 
 										if($details->leave_type === OTH){
 											echo $details->reason ;
 										}
 										?></b></p></label>
+									</div>
 								</div>
-							</div>
 
 								<div class="col s5">
 									<p>
-										<input name="leave_type" type="checkbox"<?php echo $details->leave_type ==  PL ?  "checked" : ""; ?> />
-										<label><?php echo PL;?>&nbsp; (Specify)</label>
+										<input type="checkbox"<?php echo $details->leave_type ==  PL ?  "checked" : ""; ?> />
+										<label><?php echo PL;?>&nbsp (Specify)</label>
 									</p>
 								</div>
 								<div class="col s7">
 									<div style="margin-top:-18px;">
-										<label for="reason">&nbsp;</label>
-										<label id="reason" name="reason"><p><b>&nbsp;
+										<label for="reason">&nbsp</label>
+										<label id="reason" name="reason"><p><b>&nbsp
 											<?php 
 											if($details->leave_type === PL){
 												echo $details->reason;
@@ -226,12 +227,9 @@ p{
 
 								</div>
 							</div>
-
-
 						</div>
 
 						<div class="row">
-
 							<div class="col s6">
 
 								<div class="row">
@@ -240,7 +238,6 @@ p{
 										<label><p><b><?php echo $details->total_days;?>&nbsp; days</b></p></label>
 									</div>
 								</div>
-
 								<div class="row">
 									<p>INCLUSIVE DATES</p>
 									<div class="col s6">
@@ -272,26 +269,11 @@ p{
 									<!-- </div> -->
 								</div>
 							</div>
-<<<<<<< HEAD
-						<!-- </div> -->
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<img src="<?php echo base_url()."uploads/".$details->image_file;?>" width="100" height="100" target alt="Leave Request Form Image">
-			</div>
-			<div class="row">
-				<div class="col s12" style="text-align: center;">
-					<a href="<?php echo base_url(); ?>Admin_leave/approve_leave_request/<?php echo $details->id; ?>" class="btn green darken-2">Approve</a>
-=======
-
 						</div>
-
 						<div class="row">
 							<div class="input-field col s6">
 								<img class="materialboxed" src="<?php echo base_url().'uploads/'.$details->image_file;?>" style="height: 30%; width: 30%;" alt="Leave Request Form Image">
 							</div>
-
 							<div class=" col s6">
 
 								<div class="row">
@@ -318,7 +300,6 @@ p{
 								<button type="submit" id="id_send" name="id_send" class="btn waves-effect waves-light green"> 
 									Submit
 								</button>
->>>>>>> f7769a5a91a4094af70daf67242436b16e33d10c
 
 								<a href="<?php echo base_url(); ?>Admin_leave" type="button" class="btn waves-effect waves-light orange">   
 									Cancel
@@ -326,70 +307,71 @@ p{
 								<!-- <a href="<?php echo base_url(); ?>Admin_leave/approve_leave_request/<?php echo $details->id; ?>" class="btn green darken-2">Submit</a>
 
 									<a href="<?php echo base_url(); ?>Admin_leave/disapprove_leave_request/<?php echo $details->id; ?>" class="btn orange darken-2 modal-trigger" >Cancel</a> -->
+								</div>
 							</div>
-						</div>
 
 						</form>
-			</div>
-	</div>
-	<div class="card pad-lg" >
-		<?php 
-		$vlp_details = array();
-		foreach ($user_vlp_data as $row) {
-			$vlp_details = $row;		
-		}
+					</div>
 
-		$slp_details = array();
-		foreach ($user_slp_data as $row) {
-			$slp_details = $row;		
-		}
-		?>
-		<div class="row">
-			<div class="col s6">
-				<p>Vacation Leave Points Balance &nbsp;&nbsp;&nbsp;<b><?php echo isset($vlp_details->balance) ? $vlp_details->balance : 0; ?></b></p>
-			</div>
+				</div>
+				<div class="card pad-lg" >
+					<?php 
+					$vlp_details = array();
+					foreach ($user_vlp_data as $row) {
+						$vlp_details = $row;		
+					}
 
-			<div class="col s6">
-				<p>Sick Leave Points Balance &nbsp;&nbsp;&nbsp;<b><?php echo isset($slp_details->balance) ? $slp_details->balance : 0; ?></b></p>
-			</div>
-			<div class="col s6">
-				<p>Privilege Leave Points Balance &nbsp;&nbsp;&nbsp;<b><!-- <?php echo isset($vlp_details->balance) ? $vlp_details->balance : 0; ?> -->5</b></p>
-			</div>
-			<div class="col s6">
-				<p>Maternity/Paternity Points Balance &nbsp;&nbsp;&nbsp;<b><!-- <?php echo isset($slp_details->balance) ? $slp_details->balance : 0; ?> -->0</b></p>
-			</div>
-		</div>
-	</div>
+					$slp_details = array();
+					foreach ($user_slp_data as $row) {
+						$slp_details = $row;		
+					}
+					?>
+					<div class="row">
+						<div class="col s6">
+							<p>Vacation Leave Points Balance &nbsp;&nbsp;&nbsp;<b><?php echo isset($vlp_details->balance) ? $vlp_details->balance : 0; ?></b></p>
+						</div>
 
-<script type="text/javascript">
-	$(document).ready( function() {
-			$('select').material_select();
-		});
-	$('#admin_leave_request_form').on('submit', function(e){
-			e.preventDefault();
-			var data = $(this).serialize();
-			var baseurl = "Admin_leave/approve_leave_request"
-			$.ajax({
-				url: "<?php echo base_url() ?>" +baseurl,
-				type: 'POST',
-				data: data,
-				success: function(data){
-					if($.isEmptyObject(data)){
-						swal("Success!", "Employee has been added!", "success").then(
-							function(){
-								$('#admin_leave_request_form')[0].reset();
-								window.location ="<?php echo base_url() ?>/Admin_leave";
-							})
-					}else{
-						var text = $(data).text();
-						swal("Error!",text, "error")
-					}  
-				},
-				error: function(){
-					alert('error');
-				}
-			});
-		});
+						<div class="col s6">
+							<p>Sick Leave Points Balance &nbsp;&nbsp;&nbsp;<b><?php echo isset($slp_details->balance) ? $slp_details->balance : 0; ?></b></p>
+						</div>
+						<div class="col s6">
+							<p>Privilege Leave Points Balance &nbsp;&nbsp;&nbsp;<b><!-- <?php echo isset($vlp_details->balance) ? $vlp_details->balance : 0; ?> -->5</b></p>
+						</div>
+						<div class="col s6">
+							<p>Maternity/Paternity Points Balance &nbsp;&nbsp;&nbsp;<b><!-- <?php echo isset($slp_details->balance) ? $slp_details->balance : 0; ?> -->0</b></p>
+						</div>
+					</div>
+				</div>
 
-</script>
-	
+				
+				<script type="text/javascript">
+					$(document).ready( function() {
+						$('select').material_select();
+					});
+					$('#admin_leave_request_form').on('submit', function(e){
+						e.preventDefault();
+						var data = $(this).serialize();
+						var baseurl = "Admin_leave/approve_leave_request"
+						$.ajax({
+							url: "<?php echo base_url() ?>" +baseurl,
+							type: 'POST',
+							data: data,
+							success: function(data){
+								if($.isEmptyObject(data)){
+									swal("Success!", "Employee has been added!", "success").then(
+										function(){
+											$('#admin_leave_request_form')[0].reset();
+											window.location ="<?php echo base_url() ?>/Admin_leave";
+										})
+								}else{
+									var text = $(data).text();
+									swal("Error!",text, "error")
+								}  
+							},
+							error: function(){
+								alert('error');
+							}
+						});
+					});
+
+				</script>
